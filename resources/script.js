589d93cb79d81bffa19f24c5b1a9a235a4b7d1c0
mothermushroom <mother.mushroom.shroom@protonmail.com>
2023-08-22 15:59:19 -0700

    testing drag 3

--- resources/script.js
+++ resources/script.js
@@ -34,23 +34,10 @@ $(function () {
       const swipeDistance = touchEndX - touchStartX;
 
       // You can adjust the threshold value based on your needs
-      const swipeThreshold = 100;
+      const swipeThreshold = 50;
 
       if (swipeDistance > swipeThreshold) {
         $(modal).modal('hide'); // Hide the Bootstrap modal
       }
-    });
-
-    // Bootstrap modal events
-    $(modal).on('hidden.bs.modal', () => {
-      // Reset modal content on modal hide event
-      modal.querySelector('.modal-dialog').style.left = '50%';
-      modal.querySelector('.modal-dialog').style.top = '50%';
-    });
-
-    $(modal).on('show.bs.modal', () => {
-      // Reset modal position on modal show event
-      modal.querySelector('.modal-dialog').style.left = '50%';
-      modal.querySelector('.modal-dialog').style.top = '50%';
     });
   });
