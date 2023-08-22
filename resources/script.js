88e88823cbb5db9f0954a63ad75fc1b8782bc241
mothermushroom <mother.mushroom.shroom@protonmail.com>
2023-08-22 00:12:32 -0700

    fix stupid apple no hover

--- resources/script.js
+++ resources/script.js
@@ -4,3 +4,11 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
+
+$('.popup').on('click', function() { 
+// when you click the div
+  console.log("hello")
+  $(this).addClass('no-hover'); // add the class 'no-hover'
+});
+
+console.log("hi")
