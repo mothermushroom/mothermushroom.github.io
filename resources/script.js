31bf37d5c35688ec5d66e3999be6822dc3376b46
mothermushroom <mother.mushroom.shroom@protonmail.com>
2023-08-22 13:48:43 -0700

    move x button

--- resources/script.js
+++ resources/script.js
@@ -13,37 +13,3 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
-
-
-
-
-// $('.modal').on('click', function() { 
-//   console.log("hello im here in the click")
-//     $(this).attr('btnclose').select();
-// });
-
-
-
-
-// $('.popup').on('click', function() { 
-// // when you click the div
-//   console.log("hello")
-//   if ($('.popup').hasClass('no-hover')){
-//     $('.popup').removeClass('no-hover');
-//   } else {
-//   $('.popup').addClass('no-hover')
-// }
-// });
-
-// $('div').on('click', function() { 
-// // when you click the div
-//   setTimeout(() =>{ console.log("hielo")}, 500)
-//   if ($(this).hasClass('no-hover')){
-//     $(this).removeClass('no-hover');
-//   } else {
-//   $(this).addClass('no-hover')
-// }
-
-
-// });
-
