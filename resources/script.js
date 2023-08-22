0dd3de2a02a9b9d5f8051553e1600457b371a30b
mothermushroom <mother.mushroom.shroom@protonmail.com>
2023-08-22 15:49:09 -0700

    testing drag'.

--- resources/script.js
+++ resources/script.js
@@ -13,3 +13,37 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
+
+
+
+  const modals = document.querySelectorAll('.modal');
+
+  modals.forEach(modal => {
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    modal.addEventListener('touchstart', (e) => {
+      touchStartX = e.touches[0].clientX;
+    });
+
+    modal.addEventListener('touchend', (e) => {
+      touchEndX = e.changedTouches[0].clientX;
+
+      // Determine the swipe direction and distance
+      const swipeDistance = touchEndX - touchStartX;
+
+      // You can adjust the threshold value based on your needs
+      const swipeThreshold = 100;
+
+      if (swipeDistance > swipeThreshold) {
+        $(modal).modal('hide'); // Hide the Bootstrap modal
+      }
+    });
+
+    // Bootstrap modal events
+    $(modal).on('hidden.bs.modal', () => {
+      // Reset modal content on modal hide event
+      modal.querySelector('.modal-dialog').style.left = '50%';
+      modal.querySelector('.modal-dialog').style.top = '50%';
+    });
+  });
