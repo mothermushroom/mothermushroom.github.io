c37281991f6435636a33bd15c5cc8fbed0c98686
mothermushroom <mother.mushroom.shroom@protonmail.com>
2023-08-22 10:56:47 -0700

    hover media fix css

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,12 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 $(function () {
     $(document).scroll(function () {
         var $nav = $("#mainNavbar");
@@ -5,10 +14,36 @@ $(function () {
     });
 });
 
-$('.popup').on('click', function() { 
-// when you click the div
-  console.log("hello")
-  $(this).addClass('no-hover'); // add the class 'no-hover'
-});
 
-console.log("hi")
+
+
+// $('.modal').on('click', function() { 
+//   console.log("hello im here in the click")
+//     $(this).attr('btnclose').select();
+// });
+
+
+
+
+// $('.popup').on('click', function() { 
+// // when you click the div
+//   console.log("hello")
+//   if ($('.popup').hasClass('no-hover')){
+//     $('.popup').removeClass('no-hover');
+//   } else {
+//   $('.popup').addClass('no-hover')
+// }
+// });
+
+// $('div').on('click', function() { 
+// // when you click the div
+//   setTimeout(() =>{ console.log("hielo")}, 500)
+//   if ($(this).hasClass('no-hover')){
+//     $(this).removeClass('no-hover');
+//   } else {
+//   $(this).addClass('no-hover')
+// }
+
+
+// });
+
