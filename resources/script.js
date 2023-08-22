3054ba8fe6f37a028309f31241427e494286c620
mothermushroom <mother.mushroom.shroom@protonmail.com>
2023-08-22 16:08:56 -0700

    revert drag

--- resources/script.js
+++ resources/script.js
@@ -13,31 +13,3 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
-
-
-
-
-  const modals = document.querySelectorAll('.modal');
-
-  modals.forEach(modal => {
-    let touchStartX = 0;
-    let touchEndX = 0;
-
-    modal.addEventListener('touchstart', (e) => {
-      touchStartX = e.touches[0].clientX;
-    });
-
-    modal.addEventListener('touchend', (e) => {
-      touchEndX = e.changedTouches[0].clientX;
-
-      // Determine the swipe direction and distance
-      const swipeDistance = touchEndX - touchStartX;
-
-      // You can adjust the threshold value based on your needs
-      const swipeThreshold = 50;
-
-      if (swipeDistance > swipeThreshold) {
-        $(modal).modal('hide'); // Hide the Bootstrap modal
-      }
-    });
-  });
